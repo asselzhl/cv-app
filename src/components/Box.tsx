42d62c8ff0cfe4c16f89cb5f0d4a750547b117c4
asselzhl <asselzhly@gmail.com>
2024-05-30 20:15:40 +0500

    implement inner page skeleton

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -1,32 +1,32 @@
-import React from "react";
+import React from 'react';
 
 const style = {
-  title: `text-[#009E60] font-bold text-2xl mb-8`,
+	title: `text-[#009E60] font-bold text-2xl mb-8`,
 };
 
 type BoxMapKeys =
-  | "about"
-  | "education"
-  | "experience"
-  | "skills"
-  | "portfolio"
-  | "contacts"
-  | "feedbacks";
+	| 'about'
+	| 'education'
+	| 'experience'
+	| 'skills'
+	| 'portfolio'
+	| 'contacts'
+	| 'feedbacks';
 interface BoxMapValues {
-  title: string;
-  content; // TODO: type?
-  id: BoxMapKeys;
+	title: string;
+	content; // TODO: type?
+	id: BoxMapKeys;
 }
 
 interface BoxProps {
-  box: BoxMapValues;
+	box: BoxMapValues;
 }
 
 export const Box = ({ box }: BoxProps) => {
-  return (
-    <div id={box.id}>
-      <h2 className={style.title}>{box.title}</h2>
-      {box.content}
-    </div>
-  );
+	return (
+		<div id={box.id}>
+			<h2 className={style.title}>{box.title}</h2>
+			{box.content}
+		</div>
+	);
 };
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,86 +1,86 @@
-import React from "react";
-import { HashLink } from "react-router-hash-link";
+import React from 'react';
+import { HashLink } from 'react-router-hash-link';
 
 import {
-  FaUser,
-  FaGraduationCap,
-  FaPen,
-  FaLaptopCode,
-  FaSuitcase,
-  FaAddressCard,
-  FaRegComment,
-} from "react-icons/fa6";
+	FaUser,
+	FaGraduationCap,
+	FaPen,
+	FaLaptopCode,
+	FaSuitcase,
+	FaAddressCard,
+	FaRegComment,
+} from 'react-icons/fa6';
 
 type NavItemID =
-  | "about"
-  | "education"
-  | "experience"
-  | "skills"
-  | "portfolio"
-  | "contacts"
-  | "feedbacks";
+	| 'about'
+	| 'education'
+	| 'experience'
+	| 'skills'
+	| 'portfolio'
+	| 'contacts'
+	| 'feedbacks';
 interface NavListItem {
-  icon: JSX.Element;
-  text: string;
-  id: NavItemID;
+	icon: JSX.Element;
+	text: string;
+	id: NavItemID;
 }
 
 const navList: NavListItem[] = [
-  {
-    icon: <FaUser />,
-    text: "About me",
-    id: "about",
-  },
-  {
-    icon: <FaGraduationCap />,
-    text: "Education",
-    id: "education",
-  },
-  {
-    icon: <FaPen />,
-    text: "Experience",
-    id: "experience",
-  },
-  {
-    icon: <FaLaptopCode />,
-    text: "Skills",
-    id: "skills",
-  },
-  {
-    icon: <FaSuitcase />,
-    text: "Portfolio",
-    id: "portfolio",
-  },
-  {
-    icon: <FaAddressCard />,
-    text: "Contacts",
-    id: "contacts",
-  },
-  {
-    icon: <FaRegComment />,
-    text: "Feedbacks",
-    id: "feedbacks",
-  },
+	{
+		icon: <FaUser />,
+		text: 'About me',
+		id: 'about',
+	},
+	{
+		icon: <FaGraduationCap />,
+		text: 'Education',
+		id: 'education',
+	},
+	{
+		icon: <FaPen />,
+		text: 'Experience',
+		id: 'experience',
+	},
+	{
+		icon: <FaLaptopCode />,
+		text: 'Skills',
+		id: 'skills',
+	},
+	{
+		icon: <FaSuitcase />,
+		text: 'Portfolio',
+		id: 'portfolio',
+	},
+	{
+		icon: <FaAddressCard />,
+		text: 'Contacts',
+		id: 'contacts',
+	},
+	{
+		icon: <FaRegComment />,
+		text: 'Feedbacks',
+		id: 'feedbacks',
+	},
 ];
 
 export const Navigation = () => {
-  return (
-    <nav>
-      <ul className="flex flex-col gap-y-10">
-        {navList.map((listItem) => {
-          return (
-            <li
-              key={listItem.text}
-              className="flex items-center gap-x-5 text-gray-500"
-            >
-              <HashLink smooth to={`#${listItem.id}`}>
-                {listItem.icon}
-                <span>{listItem.text}</span>
-              </HashLink>
-            </li>
-          );
-        })}
-      </ul>
-    </nav>
-  );
+	return (
+		<nav>
+			<ul className='flex flex-col gap-y-10'>
+				{navList.map((listItem) => {
+					return (
+						<li
+							key={listItem.text}
+							className='flex items-center gap-x-5 text-gray-500'
+						>
+							<HashLink smooth to={`#${listItem.id}`}>
+								{listItem.icon}
+								<span>{listItem.text}</span>
+							</HashLink>
+						</li>
+					);
+				})}
+			</ul>
+		</nav>
+	);
 };
