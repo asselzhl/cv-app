f356e5e327d705d508341318e59b7b5fd8518f27
asselzhl <asselzhly@gmail.com>
2024-05-30 21:50:05 +0500

    implement portfolio skeletopn

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,50 +1,50 @@
-import React, { useEffect, useRef, useState } from "react";
-import Isotope from "isotope-layout";
+import React, { useEffect, useRef, useState } from 'react';
+import Isotope from 'isotope-layout';
 
-import project01 from "../assets/project01.jpg";
-import project02 from "../assets/project02.png";
+import project01 from '../assets/project01.jpg';
+import project02 from '../assets/project02.png';
 
 export const Portfolio = () => {
-  const isotope = useRef<Isotope | null>();
-  const [filterKey, setFilterKey] = useState("*");
+	const isotope = useRef<Isotope | null>();
+	const [filterKey, setFilterKey] = useState('*');
 
-  useEffect(() => {
-    isotope.current = new Isotope(".filter-container", {
-      itemSelector: ".filter-item",
-      layoutMode: "fitRows",
-    });
+	useEffect(() => {
+		isotope.current = new Isotope('.filter-container', {
+			itemSelector: '.filter-item',
+			layoutMode: 'fitRows',
+		});
 
-    return () => isotope.current?.destroy();
-  }, []);
+		return () => isotope.current?.destroy();
+	}, []);
 
-  useEffect(() => {
-    if (filterKey === "*") isotope.current?.arrange({ filter: `*` });
-    else isotope.current?.arrange({ filter: `.${filterKey}` });
-  }, [filterKey]);
+	useEffect(() => {
+		if (filterKey === '*') isotope.current?.arrange({ filter: `*` });
+		else isotope.current?.arrange({ filter: `.${filterKey}` });
+	}, [filterKey]);
 
-  const handleFilterKeyChange = (key: string) => () => setFilterKey(key);
-  return (
-    <div>
-      <div>
-        <span onClick={handleFilterKeyChange("*")}>All / </span>
-        <span onClick={handleFilterKeyChange("code")}>Code / </span>
-        <span onClick={handleFilterKeyChange("ui")}>UI</span>
-      </div>
+	const handleFilterKeyChange = (key: string) => () => setFilterKey(key);
+	return (
+		<div>
+			<div>
+				<span onClick={handleFilterKeyChange('*')}>All / </span>
+				<span onClick={handleFilterKeyChange('code')}>Code / </span>
+				<span onClick={handleFilterKeyChange('ui')}>UI</span>
+			</div>
 
-      <ul className="filter-container ml-[-15px]">
-        <div className="filter-item ui">
-          <img src={project01} alt="" />
-        </div>
-        <div className="filter-item code">
-          <img src={project02} alt="" />
-        </div>
-        <div className="filter-item ui">
-          <img src={project01} alt="" />
-        </div>
-        <div className="filter-item code">
-          <img src={project02} alt="" />
-        </div>
-      </ul>
-    </div>
-  );
+			<ul className='filter-container ml-[-15px]'>
+				<div className='filter-item ui'>
+					<img src={project01} alt='' />
+				</div>
+				<div className='filter-item code'>
+					<img src={project02} alt='' />
+				</div>
+				<div className='filter-item ui'>
+					<img src={project01} alt='' />
+				</div>
+				<div className='filter-item code'>
+					<img src={project02} alt='' />
+				</div>
+			</ul>
+		</div>
+	);
 };
