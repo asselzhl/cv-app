b6fc5a748a4df3b5b5fd0fcc2b76b270be110be6
asselzhl <asselzhly@gmail.com>
2024-05-31 10:44:38 +0500

    implement Experience component

---
+++ src/components/ExperienceItem.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+const style = {
+  wrapper: `flex`,
+  leftCol: `w-[20%]`,
+  rightCol: `w-[80%]`,
+  title: `font-bold mb-2`,
+  date: `text-[#667081]`,
+};
+
+interface ExperienceInfo {
+  company: string;
+  title: string;
+  description: string;
+}
+interface ExperienceListItem {
+  date: string;
+  info: ExperienceInfo;
+}
+interface ExperienceItemProps {
+  job: ExperienceListItem;
+}
+
+export const ExperienceItem = ({ job }: ExperienceItemProps) => {
+  return (
+    <div className={style.wrapper}>
+      <div className={style.leftCol}>
+        <h4 className={style.title}>{job.info.company}</h4>
+        <h5 className={style.date}>{job.date}</h5>
+      </div>
+
+      <div className={style.rightCol}>
+        <h4 className={style.title}>{job.info.title}</h4>
+        <p>{job.info.description}</p>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/PortfolioInfo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const PortfolioInfo = () => {
+  return (
+    <div>
+      <h4 className="font-bold text-[#009E60]">Some text</h4>
+      <p>
+        Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In
+        enim justo
+      </p>
+      <a href="" className="text-[#009E60] underline">
+        View source
+      </a>
+    </div>
+  );
+};
