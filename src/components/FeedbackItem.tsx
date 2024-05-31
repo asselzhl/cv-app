e850ece9f4a756ba884e07f7f53c05c5f6d0d0fe
asselzhl <asselzhly@gmail.com>
2024-05-31 11:50:31 +0500

    implement Feedbacks component

---
+++ src/components/FeedbackItem.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const style = {
+  text: `bg-[#EEEEEE] p-3.5 mb-5`,
+  authorWrapper: `flex gap-x-5 items-center`,
+  link: `text-[#009E60]`,
+};
+
+interface FeedbacksItem {
+  text: string;
+  photo: string;
+  name: string;
+  link: string;
+}
+
+interface FeedbackItemProps {
+  feedback: FeedbacksItem;
+}
+export const FeedbackItem = ({ feedback }: FeedbackItemProps) => {
+  return (
+    <li>
+      <p className={style.text}>{feedback.text}</p>
+
+      <div className={style.authorWrapper}>
+        <img src={feedback.photo} alt={`${feedback.name}'s photo`} />
+        <span>{feedback.name},</span>
+        <a className={style.link} href={feedback.link}>
+          {feedback.link}
+        </a>
+      </div>
+    </li>
+  );
+};
