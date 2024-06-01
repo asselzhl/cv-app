57764f0d42c81a7e1d5401e7a927a0b2d41fefec
asselzhl <asselzhly@gmail.com>
2024-06-01 10:48:28 +0500

    implement toggle Panel functionality

--- src/components/Panel.tsx
+++ src/components/Panel.tsx
@@ -1,29 +1,29 @@
-import React, { useState } from "react";
-import { Navigation } from "./Navigation";
-import { PhotoBox } from "./PhotoBox";
-import { Button } from "./Button";
-import { Link } from "react-router-dom";
-import { routePaths } from "../routePaths";
+import React, { useState } from 'react';
+import { Navigation } from './Navigation';
+import { PhotoBox } from './PhotoBox';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
+import { routePaths } from '../routePaths';
 
 const style = {
-  asideClosed: `bg-[#222935] p-7 flex flex-col gap-y-14 absolute right-[100%]`,
-  asideOpen: `bg-[#222935] p-7 flex flex-col gap-y-14 relative`,
+	asideClosed: `bg-[#222935] p-7 flex flex-col gap-y-14 absolute right-[100%]`,
+	asideOpen: `bg-[#222935] p-7 flex flex-col gap-y-14 relative`,
 };
 
 export const Panel = () => {
-  const [isPanelOpen, setIsPanelOpen] = useState(false);
+	const [isPanelOpen, setIsPanelOpen] = useState(false);
 
-  const togglePanel = () => {
-    setIsPanelOpen(!isPanelOpen);
-  };
-  return (
-    <aside className={isPanelOpen ? style.asideClosed : style.asideOpen}>
-      <Button buttonConfig="togglePanel" onClick={togglePanel} />
-      <PhotoBox config="inner" />
-      <Navigation />
-      <Link to={routePaths.home}>
-        <Button buttonConfig="goBack" />
-      </Link>
-    </aside>
-  );
+	const togglePanel = () => {
+		setIsPanelOpen(!isPanelOpen);
+	};
+	return (
+		<aside className={isPanelOpen ? style.asideClosed : style.asideOpen}>
+			<Button buttonConfig='togglePanel' onClick={togglePanel} />
+			<PhotoBox config='inner' />
+			<Navigation />
+			<Link to={routePaths.home}>
+				<Button buttonConfig='goBack' />
+			</Link>
+		</aside>
+	);
 };
