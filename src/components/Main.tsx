bd9d3af5bdf8e0c6ccc37e08abec526b78073800
asselzhl <asselzhly@gmail.com>
2024-06-03 18:22:43 +0500

    implement state management

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,23 +1,23 @@
-import React from "react";
-import { Box } from "./Box";
-import { Button } from "./Button";
+import React from 'react';
+import { Box } from './Box';
+import { Button } from './Button';
 
-import { HashLink } from "react-router-hash-link";
-import { boxList } from "../helpers/lists/boxList";
+import { HashLink } from 'react-router-hash-link';
+import { boxList } from '../helpers/lists/boxList';
 
 const style = {
-  wrapepr: `p-10 flex flex-col gap-y-12 relative`,
+	wrapepr: `p-10 flex flex-col gap-y-12 relative`,
 };
 
 export const Main = () => {
-  return (
-    <div className={style.wrapepr}>
-      {boxList.map((box) => {
-        return <Box key={box.id} box={box} />;
-      })}
-      <HashLink smooth to="#top">
-        <Button buttonConfig="goToTop" />
-      </HashLink>
-    </div>
-  );
+	return (
+		<div className={style.wrapepr}>
+			{boxList.map((box) => {
+				return <Box key={box.id} box={box} />;
+			})}
+			<HashLink smooth to='#top'>
+				<Button buttonConfig='goToTop' />
+			</HashLink>
+		</div>
+	);
 };
