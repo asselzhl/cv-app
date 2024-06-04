4ea9c06325cd9b1f0ff6e70fa3c7fa4397a11e7b
asselzhl <asselzhly@gmail.com>
2024-06-04 18:19:48 +0500

    configure testing libraries

--- src/components/Education/EducationTimeline.tsx
+++ src/components/Education/EducationTimeline.tsx
@@ -1,35 +1,35 @@
-import React from "react";
+import React from 'react';
 import {
-  Timeline,
-  TimelineItem,
-  TimelineConnector,
-  TimelineHeader,
-  TimelineIcon,
-  TimelineBody,
-} from "@material-tailwind/react";
+	Timeline,
+	TimelineItem,
+	TimelineConnector,
+	TimelineHeader,
+	TimelineIcon,
+	TimelineBody,
+} from '@material-tailwind/react';
 
 interface EducationItem {
-  date: number;
-  title: string;
-  description: string;
+	date: number;
+	title: string;
+	description: string;
 }
 interface EducationItemProps {
-  education: EducationItem;
+	education: EducationItem;
 }
 
 export const EducationTimeline = ({ education }: EducationItemProps) => {
-  return (
-    <Timeline>
-      <TimelineItem>
-        <TimelineConnector />
-        <TimelineHeader className="h-3">
-          <TimelineIcon className="text-[#009E60] bg-transparent font-bold leading-none">
-            {education.date}
-          </TimelineIcon>
-          {education.title}
-        </TimelineHeader>
-        <TimelineBody className="pb-8">{education.description}</TimelineBody>
-      </TimelineItem>
-    </Timeline>
-  );
+	return (
+		<Timeline>
+			<TimelineItem>
+				<TimelineConnector />
+				<TimelineHeader className='h-3'>
+					<TimelineIcon className='text-[#009E60] bg-transparent font-bold leading-none'>
+						{education.date}
+					</TimelineIcon>
+					{education.title}
+				</TimelineHeader>
+				<TimelineBody className='pb-8'>{education.description}</TimelineBody>
+			</TimelineItem>
+		</Timeline>
+	);
 };
