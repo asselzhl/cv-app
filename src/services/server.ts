bacc94bb06641df97827cd9e0e6e5fcb578d3c30
asselzhl <asselzhly@gmail.com>
2024-06-04 11:44:30 +0500

    implement error message for server response

--- src/services/server.ts
+++ src/services/server.ts
@@ -34,7 +34,7 @@ const educations = [
 		description:
 			'Labore esse tempor nisi non mollit enim elit ullamco veniam elit duis nostrud. Enim pariatur ullamco dolor eu sunt ad velit aute eiusmod aliquip voluptate. Velit magna labore eiusmod eiusmod labore amet eiusmod. In duis eiusmod commodo duis. Exercitation Lorem sint do aliquip veniam duis elit quis culpa irure quis nulla. Reprehenderit fugiat amet sint commodo ex.\r\n',
 	},
-]
+];
 
 export const server = () =>
 	createServer({
