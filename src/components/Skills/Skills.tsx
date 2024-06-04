ef1401562d47a418760debb08f2ccb916071b9b2
asselzhl <asselzhly@gmail.com>
2024-06-04 09:53:49 +0500

    implement skill adding logic

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,39 +1,39 @@
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { stateStatus } from "../../store/constants";
-import { getSkillsList, getSkillsStateStatus } from "../../store/selectors";
-import { fetchSkills } from "../../store/skills/skillsThunk";
-import { useAppDispatch } from "../../store/store";
-import { Button } from "../Button";
-import { SkillForm } from "./SkillForm";
-import { SkillsItem } from "./SkillsItem";
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { stateStatus } from '../../store/constants';
+import { getSkillsList, getSkillsStateStatus } from '../../store/selectors';
+import { fetchSkills } from '../../store/skills/skillsThunk';
+import { useAppDispatch } from '../../store/store';
+import { Button } from '../Button';
+import { SkillForm } from './SkillForm';
+import { SkillsItem } from './SkillsItem';
 
 export const Skills = () => {
-  const dispatch = useAppDispatch();
-  const skillsList = useSelector(getSkillsList);
+	const dispatch = useAppDispatch();
+	const skillsList = useSelector(getSkillsList);
 
-  const [isFormHidden, setIsFormHidden] = useState<boolean>(true);
+	const [isFormHidden, setIsFormHidden] = useState<boolean>(true);
 
-  const skillsStateStatus = useSelector(getSkillsStateStatus);
-  useEffect(() => {
-    if (skillsStateStatus === stateStatus.idle) {
-      dispatch(fetchSkills());
-    }
-  }, [dispatch]);
+	const skillsStateStatus = useSelector(getSkillsStateStatus);
+	useEffect(() => {
+		if (skillsStateStatus === stateStatus.idle) {
+			dispatch(fetchSkills());
+		}
+	}, [dispatch]);
 
-  const toggleForm = () => {
-    setIsFormHidden(!isFormHidden);
-  };
+	const toggleForm = () => {
+		setIsFormHidden(!isFormHidden);
+	};
 
-  return (
-    <div>
-      <Button buttonConfig="openEdit" onClick={toggleForm} />
-      {!isFormHidden && <SkillForm />}
-      <ul className="flex flex-col gap-y-1">
-        {skillsList.map((skill) => {
-          return <SkillsItem key={skill.name} skill={skill} />;
-        })}
-      </ul>
-    </div>
-  );
+	return (
+		<div>
+			<Button buttonConfig='openEdit' onClick={toggleForm} />
+			{!isFormHidden && <SkillForm />}
+			<ul className='flex flex-col gap-y-1'>
+				{skillsList.map((skill) => {
+					return <SkillsItem key={skill.name} skill={skill} />;
+				})}
+			</ul>
+		</div>
+	);
 };
