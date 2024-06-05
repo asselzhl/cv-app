4d356f616a92abe81fedb9da836921d4180ed16f
asselzhl <asselzhly@gmail.com>
2024-06-05 17:46:33 +0500

    write tests

--- src/components/Box/__tests__/Box.spec.tsx
+++ src/components/Box/__tests__/Box.spec.tsx
@@ -1,39 +1,39 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
-import { Box } from "../Box";
+import { Box } from '../Box';
 
 type BoxIDs =
-  | "about"
-  | "education"
-  | "experience"
-  | "skills"
-  | "portfolio"
-  | "contacts"
-  | "feedbacks";
+	| 'about'
+	| 'education'
+	| 'experience'
+	| 'skills'
+	| 'portfolio'
+	| 'contacts'
+	| 'feedbacks';
 
 interface BoxListItem {
-  title: string;
-  content: JSX.Element;
-  id: BoxIDs;
+	title: string;
+	content: JSX.Element;
+	id: BoxIDs;
 }
 
-describe("Box component", () => {
-  const box: BoxListItem = {
-    title: "Box Title",
-    content: <p>Box Content</p>,
-    id: "about",
-  };
-  it("renders correctly with given props", () => {
-    render(<Box box={box} />);
+describe('Box component', () => {
+	const box: BoxListItem = {
+		title: 'Box Title',
+		content: <p>Box Content</p>,
+		id: 'about',
+	};
+	it('renders correctly with given props', () => {
+		render(<Box box={box} />);
 
-    const boxElement = screen.getByText("Box Content").closest("div");
-    expect(boxElement).toHaveAttribute("id", "about");
+		const boxElement = screen.getByText('Box Content').closest('div');
+		expect(boxElement).toHaveAttribute('id', 'about');
 
-    const titleElement = screen.getByText("Box Title");
-    expect(titleElement).toBeInTheDocument();
+		const titleElement = screen.getByText('Box Title');
+		expect(titleElement).toBeInTheDocument();
 
-    expect(screen.getByText("Box Content")).toBeInTheDocument();
-  });
+		expect(screen.getByText('Box Content')).toBeInTheDocument();
+	});
 });
--- src/components/Navigation/__tests__/Navigation.spec.tsx
+++ src/components/Navigation/__tests__/Navigation.spec.tsx
@@ -1,41 +1,41 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import { Navigation } from "../Navigation";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Navigation } from '../Navigation';
 
 type NavItemID =
-  | "about"
-  | "education"
-  | "experience"
-  | "skills"
-  | "portfolio"
-  | "contacts"
-  | "feedbacks";
+	| 'about'
+	| 'education'
+	| 'experience'
+	| 'skills'
+	| 'portfolio'
+	| 'contacts'
+	| 'feedbacks';
 interface NavListItem {
-  icon: JSX.Element;
-  text: string;
-  id: NavItemID;
+	icon: JSX.Element;
+	text: string;
+	id: NavItemID;
 }
 
-jest.mock("react-router-hash-link", () => ({
-  HashLink: jest.fn(({ children, ...props }) => <a {...props}>{children}</a>),
+jest.mock('react-router-hash-link', () => ({
+	HashLink: jest.fn(({ children, ...props }) => <a {...props}>{children}</a>),
 }));
 
-describe("Navigation component", () => {
-  const navList: NavListItem[] = [
-    { id: "about", text: "Section 1", icon: <svg data-testid="icon1" /> },
-    { id: "education", text: "Section 2", icon: <svg data-testid="icon2" /> },
-  ];
+describe('Navigation component', () => {
+	const navList: NavListItem[] = [
+		{ id: 'about', text: 'Section 1', icon: <svg data-testid='icon1' /> },
+		{ id: 'education', text: 'Section 2', icon: <svg data-testid='icon2' /> },
+	];
 
-  it("renders correctly with given navList", () => {
-    render(<Navigation navList={navList} />);
+	it('renders correctly with given navList', () => {
+		render(<Navigation navList={navList} />);
 
-    navList.forEach((item, index) => {
-      const icon = screen.getByTestId(`icon${index + 1}`);
-      expect(icon).toBeInTheDocument();
+		navList.forEach((item, index) => {
+			const icon = screen.getByTestId(`icon${index + 1}`);
+			expect(icon).toBeInTheDocument();
 
-      const text = screen.getByText(item.text);
-      expect(text).toBeInTheDocument();
-    });
-  });
+			const text = screen.getByText(item.text);
+			expect(text).toBeInTheDocument();
+		});
+	});
 });
