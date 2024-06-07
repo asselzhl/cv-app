af95236dc01ead41409bb1a023a621f43110abb0
asselzhl <asselzhly@gmail.com>
2024-06-07 12:24:45 +0500

    refactor Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,33 +1,33 @@
-import React from "react";
-import { buttonStyle } from "../../helpers/buttonStyleMap";
+import React from 'react';
+import { buttonStyle } from '../../helpers/buttonStyleMap';
 
-type ButtonTypes = "button" | "submit" | "reset";
+type ButtonTypes = 'button' | 'submit' | 'reset';
 
 interface ButtonProps {
-  text: string;
-  icon?: JSX.Element;
-  type?: ButtonTypes;
-  styleKey?;
-  onClick?: () => void;
+	text: string;
+	icon?: JSX.Element;
+	type?: ButtonTypes;
+	styleKey?;
+	onClick?: () => void;
 }
 
 export const Button = ({
-  text,
-  icon,
-  type = "button",
-  styleKey = "default",
-  onClick,
+	text,
+	icon,
+	type = 'button',
+	styleKey = 'default',
+	onClick,
 }: ButtonProps) => {
-  const styleConfig = buttonStyle[styleKey];
-  return (
-    <button
-      className={styleConfig.button}
-      type={type}
-      onClick={onClick}
-      data-testid="button"
-    >
-      {icon}
-      <span className={styleConfig.span}>{text}</span>
-    </button>
-  );
+	const styleConfig = buttonStyle[styleKey];
+	return (
+		<button
+			className={styleConfig.button}
+			type={type}
+			onClick={onClick}
+			data-testid='button'
+		>
+			{icon}
+			<span className={styleConfig.span}>{text}</span>
+		</button>
+	);
 };
