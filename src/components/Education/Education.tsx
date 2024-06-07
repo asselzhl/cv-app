75ccc33f18c1df5d9802b0121af00283a59a29af
asselzhl <asselzhly@gmail.com>
2024-06-07 11:13:53 +0500

    refactor code

--- src/components/Education/Education.tsx
+++ src/components/Education/Education.tsx
@@ -1,40 +1,51 @@
-import React, { useEffect } from 'react';
-import { useAppDispatch } from '../../store/store';
-import { fetchEducations } from '../../store/education/educationThunk';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from "react";
+import { useAppDispatch } from "../../store/store";
+import { fetchEducations } from "../../store/education/educationThunk";
+import { useSelector } from "react-redux";
 import {
-	getEducationsList,
-	getEducationsStateStatus,
-} from '../../store/selectors';
-import { stateStatus } from '../../store/constants';
-import { Loading } from '../Loading/Loading';
-import { EducationSucceeded } from './EducationSucceeded';
-import { Failed } from '../Failed/Failed';
+  getEducationsList,
+  getEducationsStateStatus,
+} from "../../store/selectors";
+import { stateStatus } from "../../store/constants";
+import { Loading } from "../Loading/Loading";
+
+import { Failed } from "../Failed/Failed";
+import { EducationTimeline } from "./EducationTimeline";
+
+const style = {
+  wrapper: `h-[30vh] overflow-scroll`,
+};
 
 const responseFailedMessage =
-	'Something went wrong; please review your server connection!';
+  "Something went wrong; please review your server connection!";
 
 export const Education = () => {
-	const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch();
 
-	const educationList = useSelector(getEducationsList);
-	const educationsStateStatus = useSelector(getEducationsStateStatus);
+  const educationList = useSelector(getEducationsList);
+  const educationsStateStatus = useSelector(getEducationsStateStatus);
 
-	useEffect(() => {
-		if (educationsStateStatus === stateStatus.idle) {
-			dispatch(fetchEducations());
-		}
-	}, [dispatch]);
+  useEffect(() => {
+    if (educationsStateStatus === stateStatus.idle) {
+      dispatch(fetchEducations());
+    }
+  }, [dispatch]);
 
-	if (educationsStateStatus === stateStatus.loading) {
-		return <Loading />;
-	}
+  if (educationsStateStatus === stateStatus.loading) {
+    return <Loading />;
+  }
 
-	if (educationsStateStatus === stateStatus.succeeded) {
-		return <EducationSucceeded educationList={educationList} />;
-	}
+  if (educationsStateStatus === stateStatus.failed) {
+    return <Failed text={responseFailedMessage} />;
+  }
 
-	if (educationsStateStatus === stateStatus.failed) {
-		return <Failed text={responseFailedMessage} />;
-	}
+  return (
+    <div className={style.wrapper}>
+      {educationList.map((education) => {
+        return (
+          <EducationTimeline key={education.title} education={education} />
+        );
+      })}
+    </div>
+  );
 };
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -1,50 +1,67 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { stateStatus } from '../../store/constants';
-import { getSkillsList, getSkillsStateStatus } from '../../store/selectors';
-import { fetchSkills } from '../../store/skills/skillsThunk';
-import { useAppDispatch } from '../../store/store';
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { stateStatus } from "../../store/constants";
+import { getSkillsList, getSkillsStateStatus } from "../../store/selectors";
+import { fetchSkills } from "../../store/skills/skillsThunk";
+import { useAppDispatch } from "../../store/store";
 
-import { Loading } from '../Loading/Loading';
-import { Failed } from '../Failed/Failed';
-import { SkillsSucceeded } from './SkillsSucceeded';
+import { Loading } from "../Loading/Loading";
+import { Failed } from "../Failed/Failed";
+import { buttonKeys } from "../../helpers/buttonMap";
+import { SkillForm } from "./SkillForm";
+import { SkillsItem } from "./SkillsItem";
+import { Scale } from "./Scale";
+import { Button } from "../Button/Button";
+
+const style = {
+  list: `flex flex-col gap-y-1 mb-7`,
+};
 
 const responseFailedMessage =
-	'Something went wrong; please review your server connection!';
+  "Something went wrong; please review your server connection!";
 
 export const Skills = () => {
-	const dispatch = useAppDispatch();
-	const skillsList = useSelector(getSkillsList);
+  const dispatch = useAppDispatch();
+  const skillsList = useSelector(getSkillsList);
 
-	const [isFormHidden, setIsFormHidden] = useState<boolean>(true);
+  const [isFormHidden, setIsFormHidden] = useState<boolean>(true);
 
-	const skillsStateStatus = useSelector(getSkillsStateStatus);
+  const skillsStateStatus = useSelector(getSkillsStateStatus);
 
-	useEffect(() => {
-		if (skillsStateStatus === stateStatus.idle) {
-			dispatch(fetchSkills());
-		}
-	}, [dispatch]);
+  const buttonConfig = isFormHidden
+    ? buttonKeys.openEdit
+    : buttonKeys.closeEdit;
 
-	const toggleForm = () => {
-		setIsFormHidden(!isFormHidden);
-	};
+  useEffect(() => {
+    if (skillsStateStatus === stateStatus.idle) {
+      dispatch(fetchSkills());
+    }
+  }, [dispatch]);
 
-	if (skillsStateStatus === stateStatus.loading) {
-		return <Loading />;
-	}
+  const toggleForm = () => {
+    setIsFormHidden(!isFormHidden);
+  };
 
-	if (skillsStateStatus === stateStatus.succeeded) {
-		return (
-			<SkillsSucceeded
-				skillsList={skillsList}
-				isFormHidden={isFormHidden}
-				toggleForm={toggleForm}
-			/>
-		);
-	}
+  if (skillsStateStatus === stateStatus.loading) {
+    return <Loading />;
+  }
 
-	if (skillsStateStatus === stateStatus.failed) {
-		return <Failed text={responseFailedMessage} />;
-	}
+  if (skillsStateStatus === stateStatus.failed) {
+    return <Failed text={responseFailedMessage} />;
+  }
+
+  return (
+    <div>
+      <Button buttonConfig={buttonConfig} onClick={toggleForm} />
+      {!isFormHidden && <SkillForm />}
+
+      <ul className={style.list}>
+        {skillsList.map((skill) => {
+          return <SkillsItem key={skill.name} skill={skill} />;
+        })}
+      </ul>
+
+      <Scale />
+    </div>
+  );
 };
